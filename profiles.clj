8b93bcf8a5e88f33619677acfd111aa2e50887b7
Hayley Swimelar <hayley@linbit.com>
2017-12-06 10:45:12 -0800

    update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,13 +1,14 @@
 {:user
  {:plugins [
-            [lein-ancient "0.6.14"]
             [cider/cider-nrepl "0.15.1"]
             [jonase/eastwood "0.2.5"]
+            [lein-ancient "0.6.14"]
             [lein-bikeshed "0.5.0"]
             [lein-cljfmt "0.5.7"]
             [lein-cloverage "1.0.9"]
             [lein-kibit "0.1.5"]
             [lein-pprint "1.1.2"]
+            [venantius/ultra "0.5.2"]
             ]
   :dependencies [
                  [cljfmt "0.5.7"]
